a8931b385cea22b963151f72933641d8000616be
Neal Lee <loseveny@qq.com>
2016-12-16 00:26:01 +1300

    finished the fist screen, inclued: new dynamic navbar colorful background structure adjustment some small changes

--- js/index.js
+++ js/index.js
@@ -3,9 +3,36 @@
 
 $(function(){
 	
+	//set the first screen's height
 	function setSection1Height(){
 		var h = $(window).innerHeight();
 		$('.section-1').css('height', h>1200?1200:h + 'px');
+	}
+	
+	//bind the events related to the scroll
+	function whenScroll(){
+		var winTop = $(window).scrollTop();
+		
+		//move the navbar after scroll
+		if(winTop > 10){
+			$('.navbar').addClass('move');
+			$('.top-arrow').css('opacity','1');
+		} else {
+			$('.navbar').removeClass('move');
+			$('.top-arrow').css('opacity','0');
+		}
+		
+		$(".slideanim").each(function(){
+			var pos = $(this).offset().top;
+
+			if (pos < winTop + 800) {
+				$(this).addClass("slide");
+			}
+
+			if($(this).hasClass('slide') && pos > winTop + 800){
+				$(this).removeClass('slide');
+			}
+		});
 	}
 	
 	(function initPage(){
@@ -15,10 +42,18 @@ $(function(){
 			setSection1Height();
 		});
 		
+		//initial the navbar   in case of the scrollTop != 0
+		whenScroll();
+				
 	})();
+	
+	//listen scroll
+	$(window).scroll(function() {
+		whenScroll();
+	});
 
 	// animation for go to hash position
-	$(".navbar a, footer a").on('click', function(event) {
+	$(".navbar a, footer a, .top-arrow a").on('click', function(event) {
 		if (this.hash !== "") {
 
 			event.preventDefault();
@@ -32,22 +67,6 @@ $(function(){
 				window.location.hash = hash;
 			});
 		} // End if
-	});
-
-	//control pictures' slidein
-	$(window).scroll(function() {
-		$(".slideanim").each(function(){
-			var pos = $(this).offset().top;
-
-			var winTop = $(window).scrollTop();
-			if (pos < winTop + 800) {
-				$(this).addClass("slide");
-			}
-
-			if($(this).hasClass('slide') && pos > winTop + 800){
-				$(this).removeClass('slide');
-			}
-		});
 	});
 
 	// is Detail page is availble go to detail page or go to demo page.
